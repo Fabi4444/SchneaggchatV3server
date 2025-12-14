1ec6e86ca0b8210f7f325ccb7dd3e5f4d23db27c
lerchenflo <126778614+lerchenflo@users.noreply.github.com>
2025-12-14 17:00:47 +0100

    firebase messagging debugging

--- src/main/kotlin/com/lerchenflo/schneaggchatv3server/message/MessageService.kt
+++ src/main/kotlin/com/lerchenflo/schneaggchatv3server/message/MessageService.kt
@@ -62,6 +62,7 @@ class MessageService(
         //Only for single message
         require(friendsService.areFriends(sender, receiver)) { "You can not send messages to users who are not your friends" }
 
+        println("Sendmessage: Group validation logic todo")
         //TODO: Group validation logic
         if (groupMessage) {
 
@@ -79,7 +80,10 @@ class MessageService(
         }
 
 
+        println("Sendmessage: Firebase sending")
+
         if (groupMessage) {
+            println("Group message notification not implemented")
             //TODO: Group notifiation
         }else {
             println("Firebase message send start")
